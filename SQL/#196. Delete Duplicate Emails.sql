af8c3f57817ed5c977fc8de929dc882f5f409aaa
Shushant Kumar <s.living.hell@gmail.com>
2022-11-05 12:36:56 +0530

    Adding solution for #1667 and adding another method for some questions

--- SQL/#196. Delete Duplicate Emails.sql
+++ SQL/#196. Delete Duplicate Emails.sql
@@ -1,3 +1,4 @@
+/*M1*/
 delete from Person 
 where id not in
 (
@@ -6,3 +7,6 @@ where id not in
     select email, min(id) as min_id from Person group by email
     ) temp
 );
+
+/*M2*/
+DELETE p from Person p, Person q where p.Id>q.Id AND q.Email=p.Email 
--- SQL/#627-Swap Salary.sql
+++ SQL/#627-Swap Salary.sql
@@ -1,1 +1,10 @@
+/*M1*/
 update Salary set sex = if(sex = 'f', 'm', 'f');
+
+/*M2*/
+update Salary set sex = 
+(case
+    when sex = 'f' then 'm'
+    else 'f'
+end
+);
