da5ad1bfdeb88e2b19e097d8cf2bf8e02280ec42
Shushant Kumar <s.living.hell@gmail.com>
2022-11-06 14:51:10 +0530

    Adding another method for #626

--- SQL/#626-Exchange Seats.sql
+++ SQL/#626-Exchange Seats.sql
@@ -1,8 +1,19 @@
+/*M1*/
 select 
 case
     when id%2 =0 then id-1
     when id = (select max(id) from Seat) then id
     else id+1
 end
+as id, student from Seat
+order by id asc;
+
+/*M2*/
+select 
+if(id%2=0, 
+    id-1, 
+    if(id = (select max(id) from Seat),
+        id,
+        id+1))
 as id, student from Seat
 order by id asc;
