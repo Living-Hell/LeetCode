d8d66d2dac1fe687d3ff841eaf158216ee0da68c
Shushant Kumar <s.living.hell@gmail.com>
2022-11-07 21:45:28 +0530

    Adding another method for #1581

---
+++ SQL/#1581-Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,10 @@
+select customer_id, count(customer_id) 
+as count_no_trans 
+from Visits 
+where visit_id not in (
+    select Visits.visit_id 
+    from Visits right join Transactions 
+    on Visits.visit_id = Transactions.visit_id
+    group by Visits.visit_id
+)
+group by customer_id;
