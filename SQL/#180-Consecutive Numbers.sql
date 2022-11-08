d535f950edcd095927d1308b11bc3f8a8d388a3f
Shushant Kumar <s.living.hell@gmail.com>
2022-11-08 22:39:31 +0530

    Adding another method for #180

--- SQL/#180-Consecutive Numbers.sql
+++ SQL/#180-Consecutive Numbers.sql
@@ -1,3 +1,4 @@
+/*M1*/
 select distinct l.num as ConsecutiveNums 
 from Logs l
 where l.num = 
@@ -8,3 +9,9 @@ and l.num =
 (
     select num from Logs where id = l.id +2
 )
+
+/*M2*/
+select distinct l1.num as ConsecutiveNums 
+from Logs l1, Logs l2, Logs l3
+where l2.id - l1.id = 1 and l3.id - l1.id = 2
+and l1.num = l2.num and l2.num = l3.num
