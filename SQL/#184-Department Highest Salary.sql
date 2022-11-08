fb791a11e08e175a03feee13c0309e049007c354
Shushant Kumar <s.living.hell@gmail.com>
2022-11-08 23:18:15 +0530

    Adding another method for #184

--- SQL/#184-Department Highest Salary.sql
+++ SQL/#184-Department Highest Salary.sql
@@ -1,7 +1,19 @@
+/*M1*/
 select d.name as Department, e.name as Employee, e.salary as Salary
 from Department d, Employee e
 where d.id = e.departmentId
 and (e.departmentId, e.Salary) in 
+(
+    select departmentId, max(salary) 
+    from Employee 
+    group by departmentId
+)
+
+/*M2*/
+select d.name as Department, e.name as Employee, e.salary as Salary
+from Department d right join Employee e
+on d.id = e.departmentId
+where (e.departmentId, e.Salary) in 
 (
     select departmentId, max(salary) 
     from Employee 
