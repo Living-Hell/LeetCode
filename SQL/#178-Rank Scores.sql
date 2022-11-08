4400421aaadd1ac30e56922bb0b0b89bd88881a6
Shushant Kumar <s.living.hell@gmail.com>
2022-11-08 22:42:19 +0530

    Adding another method for #178

--- SQL/#178-Rank Scores.sql
+++ SQL/#178-Rank Scores.sql
@@ -1,5 +1,13 @@
+/*M1*/
 select s1.score, count(distinct s2.score) as "rank"
 from Scores s1, Scores s2
 where s2.score >= s1.score
 group by s1.id
+order by s1.score desc;
+
+/*M2*/
+select s1.score, 
+(select count(distinct score) from Scores s2 where s2.score>=s1.score)
+as "rank"
+from Scores s1
 order by s1.score desc;
