d014079af6ec03c183cd7d657c8aca89eefa9ad3
Shushant Kumar <s.living.hell@gmail.com>
2022-11-10 20:51:37 +0530

    Adding solution for #586

--- SQL/#601-Human Traffic of Stadium.sql
+++ SQL/#601-Human Traffic of Stadium.sql
@@ -2,7 +2,7 @@ select distinct s1.*
 from Stadium s1, Stadium s2, Stadium s3
 where 
 (
-    (s1.id - s2.id) = 1 and (s1.id - s3.id) = 2 
+    s1.id - s2.id = 1 and s1.id - s3.id = 2 
     or s1.id = s2.id + 1 and s1.id = s3.id - 1
     or s1.id = s2.id - 1 and s1.id = s3.id - 2  
 )
