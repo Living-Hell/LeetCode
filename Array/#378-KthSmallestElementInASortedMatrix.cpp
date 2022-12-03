325b180b407b59f385ed06fdd5a5d1a08071764a
Shushant Kumar <s.living.hell@gmail.com>
2022-12-04 02:19:50 +0530

    Adding solution for #378

--- Array/#378-KthSmallestElementInASortedMatrix.cpp
+++ Array/#378-KthSmallestElementInASortedMatrix.cpp
@@ -17,3 +17,35 @@ public:
         
     }
 };
+
+//M2
+//Using Binary Search 
+//Faster than 57.13% (75ms)
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int s=matrix[0][0], e=matrix[n-1][n-1], mid;
+        int ans;
+
+        while(s<=e){
+            mid = s + (e-s)/2;
+            int count=0;
+            for(int i=0; i<n; i++){
+                int pos = upper_bound(matrix[i].begin(), matrix[i].end(),mid) - matrix[i].begin();
+                count+= pos;
+            }
+
+            if(count<k) s = mid+1;
+
+            else{
+                ans = mid;
+                e = mid-1;
+            }
+        }
+
+        return ans;       
+        
+    }
+};
