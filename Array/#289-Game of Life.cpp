b9d0a78d7eb218c6e8fd7a7b77e8e09a23237b94
Shushant Kumar <s.living.hell@gmail.com>
2022-12-03 13:44:52 +0530

    Adding another method for #289

--- Array/#289-Game of Life.cpp
+++ Array/#289-Game of Life.cpp
@@ -4,21 +4,10 @@
 
 class Solution {
 public:
-
-    bool validrow(int row, int m){
-        if(row>=0 && row<m) return true;
-        return false;
-    }
-
-    bool validcol(int col, int n){
-        if(col>=0 && col<n) return true;
-        return false;
-    }
-
     void gameOfLife(vector<vector<int>>& board) {
         int n = board[0].size();
         int m = board.size(); 
-        vector<vector<int>> ans = board;
+        vector<vector<int>> ans (m,vector<int>(n,0));
         vector<vector<int>> pos = {{-1,-1}, {-1,0}, {-1,1},  {0,1}, {1,1},  {1,0},  {1,-1},  {0, -1}};
 
         for(int i=0; i<m; i++){
@@ -27,13 +16,51 @@ public:
                 for(auto p : pos){
                     int r = i+p[0];
                     int c = j+p[1];
-                    if(validrow(r,m) && validcol(c,n) && board[r][c] == 1)
+                    if(r>=0 && r<m && c>=0 && c<n && board[r][c] == 1)
                         count1++;
                 }
-                if(board[i][j] == 1 && (count1<2 || count1>3)) ans[i][j]=0;
+                if(board[i][j] == 1){
+                    if(count1<2 || count1>3) 
+                        ans[i][j]=0;
+                    else
+                        ans[i][j]=1;
+                }
                 if(board[i][j] == 0 && count1==3) ans[i][j]=1;
             }
         }
         board = ans;
+    }
+};
+
+//M2
+//Simulation and Iteration using O(1) space
+//Faster than 53.64% (3ms)
+
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int n = board[0].size();
+        int m = board.size(); 
+        vector<vector<int>> pos = {{-1,-1}, {-1,0}, {-1,1},  {0,1}, {1,1},  {1,0},  {1,-1},  {0, -1}};
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int count1 = 0;
+                for(auto p : pos){
+                    int r = i+p[0];
+                    int c = j+p[1];
+                    if(r>=0 && r<m && c>=0 && c<n && (board[r][c] == 1 || board[r][c] == 2))
+                        count1++;
+                }
+                if(board[i][j] == 1){
+                    if(count1<2 || count1>3) 
+                        board[i][j]=2;
+                }
+                if(board[i][j] == 0 && count1==3) board[i][j]=3;
+            }
+        }
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                board[i][j]%=2;
     }
 };
