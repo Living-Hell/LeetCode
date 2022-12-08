31ace0f10afd40c5aa196856cfa2d93cc0999779
Shushant Kumar <s.living.hell@gmail.com>
2022-12-09 01:50:28 +0530

    Adding solution for #64

---
+++ Array/#64-MinimumPathSum.cpp
@@ -0,0 +1,28 @@
+//M1
+//Using DP
+//Faster than 43.77% (22ms)
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+
+        int n = grid.size(), m = grid[0].size();
+
+        for(int j=0; j<m; j++){
+            for(int i=0; i<n; i++){
+
+                if(i==0 and j==0) continue;
+                
+                int up = INT_MAX;
+                if(i>0) up = grid[i-1][j];
+                
+                int left = INT_MAX;
+                if(j>0) left = grid[i][j-1];
+
+                grid[i][j] += min(up,left);
+            }
+        }
+
+        return grid[n-1][m-1];
+    }
+};
