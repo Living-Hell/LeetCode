39524226d930c04e40b5cf694fb56be9b2c619e9
Shushant Kumar <s.living.hell@gmail.com>
2023-01-25 10:37:50 +0530

    Adding another method for #32

--- String/#32-LongestValidParentheses.cpp
+++ String/#32-LongestValidParentheses.cpp
@@ -22,3 +22,30 @@ public:
         return *max_element(dp.begin(),dp.end());
     }
 };
+
+//M2
+//Using Stacks
+//Faster than 79.93%(4ms)
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        stack<int> st;
+        st.push(-1);
+        int ans=0;
+
+        for(int i=0; i<n; i++){
+            if(s[i] == ')'){
+                st.pop();
+                if(st.empty())
+                    st.push(i);
+                else{
+                    ans = max(ans, i-st.top());
+                }
+            }
+            else st.push(i);
+        }
+        return ans;
+    }
+};
