cd039a8987f2efe76d1445c51e886bb2f1ccadf9
Shushant Kumar <s.living.hell@gmail.com>
2023-09-04 23:07:08 +0530

    Adding 116

---
+++ Tree/#116-Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,48 @@
+// M1
+// Using BFS through queue
+// Faster than 37.89% (21ms)
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (!root)
+            return root;
+        queue<pair<Node *, int>> q;
+        q.push({root, 0});
+        while (!q.empty())
+        {
+            auto top = q.front();
+            q.pop();
+            int lvl = top.second;
+            Node *node = top.first;
+            if (node->left)
+            {
+                q.push({node->left, lvl + 1});
+                q.push({node->right, lvl + 1});
+            }
+            if (!q.empty() and q.front().second == lvl)
+                node->next = q.front().first;
+        }
+        return root;
+    }
+};
