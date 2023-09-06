f1a929e9d9755b34ce4f0cc47537e54e5961c345
Shushant Kumar <s.living.hell@gmail.com>
2023-09-07 01:55:19 +0530

    Adding M2 for 187

--- Hashmap/#187-Repeated DNA Sequences.cpp
+++ Hashmap/#187-Repeated DNA Sequences.cpp
@@ -45,3 +45,27 @@ public:
         return ans;
     }
 };
+
+// M2
+// Using 2 pointer and hashmap
+// Faster than 93.73% (35 ms)
+
+class Solution
+{
+public:
+    vector<string> findRepeatedDnaSequences(string s)
+    {
+        int n = s.size();
+        vector<string> ans;
+        unordered_map<string, int> mp;
+        for (int i = 0; i < n - 9; i++)
+        {
+            string t = s.substr(i, 10);
+            mp[t]++;
+        }
+        for (auto i : mp)
+            if (i.second > 1)
+                ans.push_back(i.first);
+        return ans;
+    }
+};
