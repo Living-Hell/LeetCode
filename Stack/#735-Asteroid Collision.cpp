ee9c41fb830fe648b5f4dab6e156f6d0ec72def1
Shushant Kumar <s.living.hell@gmail.com>
2023-09-19 17:34:14 +0530

    Adding 735

---
+++ Stack/#735-Asteroid Collision.cpp
@@ -0,0 +1,35 @@
+// M1
+// Using Stack
+// Faster than 98.52% (4ms)
+
+class Solution
+{
+public:
+    vector<int> asteroidCollision(vector<int> &asteroids)
+    {
+        stack<int> st;
+        int n = asteroids.size();
+        for (const int &i : asteroids)
+        {
+            if (st.size() == 0 or i > 0)
+                st.push(i);
+            else
+            {
+                while (st.size() > 0 and st.top() > 0 and st.top() < abs(i))
+                    st.pop();
+                if (st.size() and st.top() == abs(i))
+                    st.pop();
+                else
+                {
+                    if (st.size() == 0 or st.top() < 0)
+                        st.push(i);
+                }
+            }
+        }
+        vector<int> ans;
+        while (st.size())
+            ans.push_back(st.top()), st.pop();
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
