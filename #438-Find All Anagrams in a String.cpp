de8e789a69c3322478ad6f34f78ce3f4fce96fed
Shushant Kumar <s.living.hell@gmail.com>
2023-09-23 23:59:29 +0530

    Adding 567 and 438

--- #438-Find All Anagrams in a String.cpp
+++ #438-Find All Anagrams in a String.cpp
@@ -34,3 +34,52 @@ public:
         return 0;
     }
 };
+
+// M2
+// Using 2-Pointers and frequency table
+// Faster than 86.17% (8ms)
+
+class Solution
+{
+public:
+    vector<int> findAnagrams(string s2, string s1)
+    {
+        int n1 = s1.size(), n2 = s2.size();
+        if (n1 > n2)
+            return {};
+        vector<int> ans;
+        vector<int> f1(26, 0), f2(26, 0);
+        for (int i = 0; i < n1; i++)
+        {
+            f1[s1[i] - 'a']++;
+            f2[s2[i] - 'a']++;
+        }
+        int count = 0;
+        for (int i = 0; i < 26; i++)
+            count += (f1[i] == f2[i]);
+        int l = 0, r = n1;
+        while (r < n2)
+        {
+            if (count == 26)
+                ans.push_back(l);
+            int c = s2[r] - 'a';
+            f2[c]++;
+            if (f1[c] == f2[c])
+                count++;
+            else if (f1[c] == f2[c] - 1)
+                count--;
+
+            c = s2[l] - 'a';
+            f2[c]--;
+            if (f1[c] == f2[c])
+                count++;
+            else if (f1[c] == f2[c] + 1)
+                count--;
+
+            l++, r++;
+        }
+        if (count == 26)
+            ans.push_back(l);
+        return ans;
+    }
+};
--- #567-Permutation in String.cpp
+++ #567-Permutation in String.cpp
@@ -73,5 +73,47 @@ public:
 };
 
 // M3
-// Using Brute Force
-// Faster than % (ms)
+// Using 2-Pointers and frequency table
+// Faster than 56.11% (8ms)
+
+class Solution
+{
+public:
+    bool checkInclusion(string s1, string s2)
+    {
+        int n1 = s1.size(), n2 = s2.size();
+        if (n1 > n2)
+            return 0;
+        vector<int> f1(26, 0), f2(26, 0);
+        for (int i = 0; i < n1; i++)
+        {
+            f1[s1[i] - 'a']++;
+            f2[s2[i] - 'a']++;
+        }
+        int count = 0;
+        for (int i = 0; i < 26; i++)
+            count += (f1[i] == f2[i]);
+        int l = 0, r = n1;
+        while (r < n2)
+        {
+            if (count == 26)
+                return 1;
+            int c = s2[r] - 'a';
+            f2[c]++;
+            if (f1[c] == f2[c])
+                count++;
+            else if (f1[c] == f2[c] - 1)
+                count--;
+
+            c = s2[l] - 'a';
+            f2[c]--;
+            if (f1[c] == f2[c])
+                count++;
+            else if (f1[c] == f2[c] + 1)
+                count--;
+
+            l++, r++;
+        }
+        return count == 26;
+    }
+};
