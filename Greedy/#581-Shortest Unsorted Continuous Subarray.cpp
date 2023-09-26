fdd41be23c03325c3eadefd5ee3ea920bce8aa00
Shushant Kumar <s.living.hell@gmail.com>
2023-09-27 00:43:53 +0530

    Adding M2 for #581

--- Greedy/#581-Shortest Unsorted Continuous Subarray.cpp
+++ Greedy/#581-Shortest Unsorted Continuous Subarray.cpp
@@ -29,3 +29,33 @@ public:
         return (st == -1 and end == n) ? 0 : end - st + 1;
     }
 };
+
+// M1
+// Using greedy approach
+// Faster than 66.04% (23 ms)
+class Solution
+{
+public:
+    int findUnsortedSubarray(vector<int> &nums)
+    {
+        int n = nums.size(), st = -1, end = n;
+        int mine = INT_MAX, maxe = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            maxe = max(maxe, nums[i]);
+            if (nums[i] != maxe)
+            {
+                end = i;
+            }
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            mine = min(mine, nums[i]);
+            if (nums[i] != mine)
+            {
+                st = i;
+            }
+        }
+        return (st == -1 and end == n) ? 0 : end - st + 1;
+    }
+};
