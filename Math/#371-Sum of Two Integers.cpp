ec6629f8afe9f6c3c3923527ec1bc3e86894dfed
Shushant Kumar <s.living.hell@gmail.com>
2023-10-09 00:13:43 +0530

    Added #371

--- Math/#371-Sum of Two Integers.cpp
+++ Math/#371-Sum of Two Integers.cpp
@@ -10,3 +10,22 @@ public:
         return log2(pow(2, a) * pow(2, b));
     }
 };
+
+// M2
+// Using bitwise operators
+// Faster than 55.2% (2ms)
+
+class Solution
+{
+public:
+    int getSum(int a, int b)
+    {
+        while (b != 0)
+        {
+            unsigned int carry = a & b;
+            a ^= b;
+            b = carry << 1;
+        }
+        return a;
+    }
+};
