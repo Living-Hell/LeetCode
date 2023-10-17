1941f647ec1b7b99f3ec1ace4c43dbf5b49e88cd
Shushant Kumar <s.living.hell@gmail.com>
2023-10-17 23:59:05 +0530

    Added #939

--- Array/#939-Minimum Area Rectangle.cpp
+++ Array/#939-Minimum Area Rectangle.cpp
@@ -28,3 +28,39 @@ public:
         return ans == INT_MAX ? 0 : ans;
     }
 };
+
+// M2
+// Using hashmap and set
+// Faster than 57.65% (359 ms)
+
+class Solution
+{
+public:
+    int minAreaRect(vector<vector<int>> &points)
+    {
+        int n = points.size();
+        unordered_map<int, set<int>> mp;
+        int ans = INT_MAX;
+
+        for (auto i : points)
+            mp[i[0]].insert(i[1]);
+
+        for (int i = 0; i < n; i++)
+        {
+            int x1 = points[i][0], y1 = points[i][1];
+            for (int j = i + 1; j < n; j++)
+            {
+                int x2 = points[j][0], y2 = points[j][1];
+                if (x1 == x2 or y1 == y2)
+                    continue;
+                if (mp[x1].count(y2) and mp[x2].count(y1))
+                {
+                    int area = abs(x1 - x2) * abs(y1 - y2);
+                    if (area)
+                        ans = min(ans, area);
+                }
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
