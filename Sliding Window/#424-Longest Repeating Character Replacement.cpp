1995f3060d74cf45872764ff565e7b6c67854d91
Shushant Kumar <s.living.hell@gmail.com>
2023-10-20 11:14:30 +0530

    Added #424

--- Sliding Window/#424-Longest Repeating Character Replacement.cpp
+++ Sliding Window/#424-Longest Repeating Character Replacement.cpp
@@ -27,3 +27,31 @@ public:
         return ans;
     }
 };
+
+// M2
+// Using sliding window and frequency table
+// Faster than 84.44% (6ms)
+class Solution
+{
+public:
+    int characterReplacement(string s, int k)
+    {
+        int n = s.size();
+        int i = 0, j = 0, ans = 0;
+        int a[26] = {0};
+        int maxf = 0;
+        while (j < n)
+        {
+            a[s[j] - 'A']++;
+            maxf = max(maxf, a[s[j] - 'A']);
+            if (j - i + 1 - maxf > k)
+            {
+                while (j - i + 1 - maxf > k)
+                    a[s[i++] - 'A']--;
+            }
+            ans = max(ans, j - i + 1);
+            j++;
+        }
+        return ans;
+    }
+};
