81ed67fd0bfad6dcc03dd628d039ec87ad511c71
Shushant Kumar <s.living.hell@gmail.com>
2023-10-20 22:04:12 +0530

    Added #1004

--- Sliding Window/#1004-Max Consecutive Ones III.cpp
+++ Sliding Window/#1004-Max Consecutive Ones III.cpp
@@ -27,3 +27,50 @@ public:
         return ans;
     }
 };
+
+// M2
+// Using Binary Search
+// Faster than 6.79% (61ms)
+
+class Solution
+{
+public:
+    bool checker(vector<int> &nums, int k, int mid)
+    {
+        int n = nums.size();
+        int freq = 0;
+        for (int i = 0; i < mid; i++)
+        {
+            if (nums[i])
+                freq++;
+        }
+        if (mid - freq <= k)
+            return 1;
+
+        for (int i = mid; i < n; i++)
+        {
+            freq = freq - nums[i - mid] + nums[i];
+            if (mid - freq <= k)
+                return 1;
+        }
+        return 0;
+    }
+    int longestOnes(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int ans = 0;
+        int st = 0, end = n;
+        while (st <= end)
+        {
+            int mid = st + (end - st) / 2;
+            if (checker(nums, k, mid))
+            {
+                ans = max(ans, mid);
+                st = mid + 1;
+            }
+            else
+                end = mid - 1;
+        }
+        return ans;
+    }
+};
