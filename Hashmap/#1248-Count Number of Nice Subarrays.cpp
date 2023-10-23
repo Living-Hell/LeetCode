473ab28fc176f730c4177ac84b8fe2c55d3db90a
Shushant Kumar <s.living.hell@gmail.com>
2023-10-24 00:17:27 +0530

    Added #1248

--- Hashmap/#1248-Count Number of Nice Subarrays.cpp
+++ Hashmap/#1248-Count Number of Nice Subarrays.cpp
@@ -33,3 +33,41 @@ public:
         return count;
     }
 };
+
+// M2
+// Using Sliding Window
+// Faster than 47.88% (115ms)
+
+class Solution
+{
+public:
+    int countSub(vector<int> &nums, int k)
+    {
+        int count = 0, res = 0;
+        int n = nums.size();
+        int i = 0, j = 0;
+        while (j < n)
+        {
+            count += nums[j];
+            while (count > k)
+            {
+                count -= nums[i++];
+            }
+            res += j - i + 1;
+            j++;
+        }
+        return res;
+    }
+    int numberOfSubarrays(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] & 1)
+                nums[i] = 1;
+            else
+                nums[i] = 0;
+        }
+        return countSub(nums, k) - countSub(nums, k - 1);
+    }
+};
