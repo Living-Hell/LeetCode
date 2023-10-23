c436ffa911edef3c21b5b4b374ab103e9deac9ef
Shushant Kumar <s.living.hell@gmail.com>
2023-10-23 11:57:33 +0530

    Added #1029

--- Recursion/#1029-Two City Scheduling.cpp
+++ Recursion/#1029-Two City Scheduling.cpp
@@ -34,3 +34,22 @@ public:
         return solve(costs, 0, n, n);
     }
 };
+
+// M2
+// Using sorting
+// Faster than 42.33% (6ms)
+class Solution
+{
+public:
+    int twoCitySchedCost(vector<vector<int>> &costs)
+    {
+        int n = costs.size() / 2, ans = 0;
+        sort(costs.begin(), costs.end(), [&](const vector<int> &a, const vector<int> &b)
+             { return a[0] - a[1] < b[0] - b[1]; });
+        for (int i = 0; i < n; i++)
+        {
+            ans += costs[i][0] + costs[i + n][1];
+        }
+        return ans;
+    }
+};
