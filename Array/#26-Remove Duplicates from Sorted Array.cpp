82b3579e3ab3513f0dc330976430ad38f3fbc77f
Shushant Kumar <s.living.hell@gmail.com>
2023-11-07 16:48:15 +0530

    Added #26

---
+++ Array/#26-Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,22 @@
+// M1
+// Using unordered sets
+// Faster than 42.75% (11ms)
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        unordered_set<int> st;
+        int ind = 0, n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (!st.count(nums[i]))
+            {
+                nums[ind++] = nums[i];
+                st.insert(nums[i]);
+            }
+        }
+        return ind;
+    }
+};
