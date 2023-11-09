dfe191e09179adce1c3997a71edae5d53d95b3c9
Shushant Kumar <s.living.hell@gmail.com>
2023-11-09 09:50:15 +0530

    Added #238

--- Array/#238-Product of Array Except Self.cpp
+++ Array/#238-Product of Array Except Self.cpp
@@ -32,3 +32,52 @@ public:
         return ans;
     }
 };
+
+// M2
+// Using prefix and suffix product
+// Faster than 84.97% (15 ms)
+
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int n = nums.size(), countz = 0, tprod = 1;
+        for (int i : nums)
+        {
+            if (i == 0)
+                countz++;
+            else
+                tprod *= i;
+        }
+        vector<int> ans(n);
+        if (countz == 1)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == 0)
+                    ans[i] = tprod;
+            }
+        }
+        else if (countz == 0)
+        {
+            ans[n - 1] = 1;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                ans[i] = ans[i + 1] * nums[i + 1];
+            }
+            int pref = 1;
+            for (int i = 1; i < n; i++)
+            {
+                pref *= nums[i - 1];
+                ans[i] *= pref;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+                ans[i] = 0;
+        }
+        return ans;
+    }
+};
