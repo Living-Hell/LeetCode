9afa529f2e5d16bc437bd65679aea92e757cd724
Shushant Kumar <s.living.hell@gmail.com>
2023-11-13 20:54:26 +0530

    Added #1581

--- SQL/#1581-Customer Who Visited but Did Not Make Any Transactions.sql
+++ SQL/#1581-Customer Who Visited but Did Not Make Any Transactions.sql
@@ -8,3 +8,8 @@ where visit_id not in (
     group by Visits.visit_id
 )
 group by customer_id;
+
+-- M2
+select customer_id, count(customer_id) as count_no_trans  from Visits
+where visit_id not in (select visit_id from Transactions)
+group by customer_id;
