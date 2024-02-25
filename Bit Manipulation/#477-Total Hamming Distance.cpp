4c8e82bc4cf2e3675461f97254c15e4f1b6cd70e
Shushant Kumar <shushant@accuknox.com>
2024-02-25 22:44:16 +0530

    Adding #477 in Bit Manipulation

--- Bit Manipulation/#477-Total Hamming Distance.cpp
+++ Bit Manipulation/#477-Total Hamming Distance.cpp
@@ -7,7 +7,7 @@ class Solution
 public:
     int totalHammingDistance(vector<int> &nums)
     {
-        int sum = 0, n = nums.size();
+        int n = nums.size(), sum = 0;
         int a[32] = {0};
 
         for (int i = 0; i < 32; i++)
