4ecd9d1a50d75f5809e4d9198604b3b0887cfd3f
Shushant Kumar <shushant@accuknox.com>
2024-02-25 23:16:01 +0530

    Adding #225 in Queue

--- Queue/#225-Implement Stack using Queues.cpp
+++ Queue/#225-Implement Stack using Queues.cpp
@@ -55,11 +55,52 @@ public:
     }
 };
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
+// M2
+// Using 1 Queue
+// Faster than 100% (0 ms)
+
+class MyStack
+{
+public:
+    queue<int> q;
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        q.push(x);
+    }
+
+    int pop()
+    {
+        int n = q.size();
+        while (--n)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        int res = q.front();
+        q.pop();
+        return res;
+    }
+
+    int top()
+    {
+        int n = q.size();
+        while (--n)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        int res = q.front();
+        q.pop();
+        q.push(res);
+        return res;
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
