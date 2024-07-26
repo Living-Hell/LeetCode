c43a2ef58543cd15414430583a9b4e029392a1fe
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-07-26 23:44:34 +0530

    Updating #834

--- Graph/#834-Sum of Distances in Tree.cpp
+++ Graph/#834-Sum of Distances in Tree.cpp
@@ -42,4 +42,37 @@ public:
 
 //M2
 //Using DFS and root shifting technique
-//Faster than % ( ms)
+//Faster than 95.16% (178ms)
+class Solution {
+public:
+    void dfs(vector<vector<int>> &adj, vector<int> &res, vector<int> &counter, int node, int prevNode = -1){
+        for(auto ad : adj[node]){
+            if(ad == prevNode) continue;
+            dfs(adj,res,counter,ad,node);
+            counter[node] += counter[ad];
+            res[node] += res[ad] + counter[ad];
+        }
+        counter[node] += 1;
+    }
+    void dfs2(vector<vector<int>> &adj, vector<int> &res, vector<int> &counter, int node, int n, int prevNode = -1){
+        for(auto ad : adj[node]){
+            if(ad == prevNode) continue;
+            res[ad] = res[node] - counter[ad] + n - counter[ad];
+            dfs2(adj,res,counter,ad,n, node);
+        }
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        vector<int> res(n,0), counter(n,0);
+        vector<vector<int>> adj(n);
+        for(auto &e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        dfs(adj,res,counter,0);
+        dfs2(adj,res,counter,0,n);
+
+        return res;
+
+    }
+};
