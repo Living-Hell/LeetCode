f61d629de76be248dfc22d54161c8053d68d083f
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-07-31 21:07:23 +0530

    Updating #1653

--- String/#1653-Minimum Deletions to Make String Balanced.cpp
+++ String/#1653-Minimum Deletions to Make String Balanced.cpp
@@ -25,3 +25,32 @@ public:
         return res;
     }
 };
+
+//M2
+//Using prefix sum
+//Faster than 38.5% (105ms)
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.size();
+        int res = INT_MAX, counta = 0;
+        vector<int> freqa(n), freqb(n);
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a')
+                counta++;
+            freqa[i] = counta;
+            freqb[i] = i+1 - counta;
+        }
+
+        if (!counta or counta == n)
+            return 0;
+
+        for (int i = 0; i < n; i++) {
+            res = min(res, freqb[i] + freqa[n - 1] - freqa[i] - (s[i] == 'b'));
+        }
+
+        return res;
+    }
+};
