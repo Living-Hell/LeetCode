050694480ac6a7af5731fe882bbd3ed399677b12
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-08-05 20:45:34 +0530

    Adding #2053

---
+++ String/#2053-Kth Distinct String in an Array.cpp
@@ -0,0 +1,20 @@
+//M1
+//Using hashmap
+//Faster than 59.81% (16ms)
+
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> mp;
+        for (string& s : arr)
+            mp[s]++;
+        for (string& s : arr) {
+            if (mp[s] == 1) {
+                k--;
+                if (k == 0)
+                    return s;
+            }
+        }
+        return "";
+    }
+};
