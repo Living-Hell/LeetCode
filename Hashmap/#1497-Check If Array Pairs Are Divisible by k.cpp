2cdaae087b9032da19f98a6b44865ad64d917b91
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-10-02 00:17:57 +0530

    Addding #1497

--- Hashmap/#1497-Check If Array Pairs Are Divisible by k.cpp
+++ Hashmap/#1497-Check If Array Pairs Are Divisible by k.cpp
@@ -1,3 +1,25 @@
 //M1
 //Using hashmap
-//Faster than % (ms)
+//Faster than 21.43% (155 ms)
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+        for (int& i : arr) {
+            mp[((i % k) + k) % k]++;
+        }
+
+        for (int i = 0; i < k; i++) {
+            if (i == 0) {
+                if (mp[i] & 1) {
+                    return false;
+                }
+                continue;
+            }
+            if (mp[i] != mp[k - i])
+                return false;
+        }
+        return true;
+    }
+};
