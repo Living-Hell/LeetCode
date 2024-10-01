143f995605f1e413e0dc4bb5ae826942a203d496
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-10-02 00:19:53 +0530

    Adding M2 for #1497

--- Hashmap/#1497-Check If Array Pairs Are Divisible by k.cpp
+++ Hashmap/#1497-Check If Array Pairs Are Divisible by k.cpp
@@ -23,3 +23,30 @@ public:
         return true;
     }
 };
+
+//M2
+//Using hashmaps
+//Faster than 71.3% (117ms)
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+        for (int& i : arr) {
+            mp[((i % k) + k) % k]++;
+        }
+
+        for (auto &elm:mp) {
+            int i = elm.first;
+            if (i == 0) {
+                if (mp[i] & 1) {
+                    return false;
+                }
+                continue;
+            }
+            if (mp[i] != mp[k - i])
+                return false;
+        }
+        return true;
+    }
+};
