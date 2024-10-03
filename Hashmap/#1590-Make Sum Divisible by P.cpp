558f01d4a286e224da1c9c9141756afa49f28bf4
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-10-04 00:13:20 +0530

    Adding M2 for #1590

--- Hashmap/#1590-Make Sum Divisible by P.cpp
+++ Hashmap/#1590-Make Sum Divisible by P.cpp
@@ -27,3 +27,36 @@ public:
         return res == n ? -1 : res;
     }
 };
+
+//M2
+//Using hashmap and sliding window
+//Faster than 34.7% (143ms)
+
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        nums[0] %= p;
+        for (int i = 1; i < n; i++) {
+            nums[i] = (nums[i] % p + nums[i - 1]) % p;
+        }
+        unordered_map<int, int> mp;
+        int res = n, rem = nums[n - 1];
+
+        if (rem == 0)
+            return rem;
+
+        mp[0] = -1;
+
+        for (int i = 1; i < n; i++) {
+            int currRem = (p - (rem - nums[i - 1])) % p;
+            if (mp.contains(currRem)) {
+                res = min(res, i - mp[currRem] - 1);
+            }
+            if (nums[i - 1] == 0)
+                res = min(res, n - i);
+            mp[nums[i - 1]] = i - 1;
+        }
+        return res == n ? -1 : res;
+    }
+};
