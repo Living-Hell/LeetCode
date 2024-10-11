d1817f5309dc5daefe892addaf35b98e65f07e50
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-10-12 02:23:12 +0530

    Adding M2 for #1942

--- Priority Queue/#1942-The Number of the Smallest Unoccupied Chair.cpp
+++ Priority Queue/#1942-The Number of the Smallest Unoccupied Chair.cpp
@@ -45,3 +45,47 @@ public:
         return -1;
     }
 };
+
+//M2
+//Using 2 priority queues (min heap) and tuple
+//Faster than 95.56 (124ms)
+
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        vector<tuple<int, int, int>> v;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        priority_queue<int,vector<int>,greater<int>> avlChairs;
+
+        int i = 0, n = times.size(), allotted = 0;
+        for (int i = 0; i < n; i++) {
+            v.push_back({times[i][0], times[i][1], i});
+        }
+        sort(v.begin(), v.end());
+        while (i < n) {
+            int arriveTime = get<0>(v[i]);
+            int leaveTime = get<1>(v[i]);
+            int frnd = get<2>(v[i]);
+
+            while(!pq.empty() and get<0>(pq.top()) <= get<0>(v[i])) {
+                int freedChair = get<1>(pq.top());
+                pq.pop();
+                avlChairs.push(freedChair);
+            }
+            int chairToGive;
+            if(!avlChairs.empty()){
+                chairToGive = avlChairs.top();
+                avlChairs.pop();
+            } else {
+                chairToGive = allotted++;
+            }
+
+            if(frnd == targetFriend)
+                return chairToGive;
+            
+            pq.push({leaveTime,chairToGive,frnd});
+            i++;
+        }
+        return -1;
+    }
+};
